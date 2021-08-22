ebb9e494eb5a9d96934a90d6ba24a8d1e2efab77
LucasPickering <lucas.pickering@markforged.com>
2021-08-22 18:54:39 +0000

    Deploying to gh-pages from  @ 8f3a1d994ee123ecba7bad1b40417da8f029202d 🚀

--- src/components/Narrow.tsx
+++ src/components/Narrow.tsx
@@ -2,17 +2,17 @@ import styles from "@root/styles/Narrow.module.css";
 import clsx from "clsx";
 
 interface Props {
-  className?: string;
   enabled?: boolean;
 }
 
 /**
  * A layout wrapper to center a component and limit its width to a center column.
  */
-const Narrow: React.FC<Props> = ({ className, enabled = true, children }) => (
-  <div className={clsx(className, enabled && styles.narrowContent)}>
-    {children}
-  </div>
-);
+const Narrow: React.FC<Props> = ({ enabled = true, children }) =>
+  enabled ? (
+    <div className={clsx(styles.narrowContent)}>{children}</div>
+  ) : (
+    <>{children}</>
+  );
 
 export default Narrow;
