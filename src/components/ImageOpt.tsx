4d61eb885a61ce9e29d1d2bb308b2cce15ca6440
LucasPickering <lucas.pickering@markforged.com>
2021-08-27 22:45:43 +0000

    Deploying to gh-pages from  @ 19c83f40f0dcb700fa77ae96e656eedf39f05b98 🚀

--- src/components/ImageOpt.tsx
+++ src/components/ImageOpt.tsx
@@ -4,11 +4,24 @@ import { Image, Placeholder, Transformation } from "cloudinary-react";
 /**
  * An optimized image. Should be used for all rendered images.
  */
-const ImageOpt: React.FC<React.ComponentProps<typeof Image>> = (props) => (
-  <Image loading="lazy" {...props}>
-    <Placeholder />
-    <Transformation quality="auto" fetchFormat="auto" />
-  </Image>
-);
+const ImageOpt: React.FC<React.ComponentProps<typeof Image>> = ({
+  className,
+  publicId,
+  ...rest
+}) => {
+  // We allow local paths here too, for iterating during writing. In production,
+  // we should *always* server images from the CDN
+  if (process.env.NODE_ENV === "development" && publicId.startsWith("/")) {
+    // eslint-disable-next-line jsx-a11y/alt-text
+    return <img className={className} src={publicId} />;
+  }
+
+  return (
+    <Image loading="lazy" className={className} publicId={publicId} {...rest}>
+      <Placeholder />
+      <Transformation quality="auto" fetchFormat="auto" />
+    </Image>
+  );
+};
 
 export default ImageOpt;
