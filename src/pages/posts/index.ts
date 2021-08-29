3c9d999f64a97793022c504218dacb12b4fdeb39
LucasPickering <lucas.pickering@markforged.com>
2021-08-29 00:26:34 +0000

    Deploying to gh-pages from  @ 2a85bdd1008fb8bdb020126477ae1fa87ec8fb89 🚀

--- src/pages/posts/index.ts
+++ src/pages/posts/index.ts
@@ -3,6 +3,7 @@
  * I couldn't figure out how to dynamically list+load all the MDX files.
  */
 
+export * as bostonConcrete from "./boston-concrete.mdx";
 export * as laulud from "./laulud.mdx";
 export * as mtWashington from "./mt-washington.mdx";
 export * as soze from "./soze.mdx";
