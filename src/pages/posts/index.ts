0f72d97fe6dc8813c1bc9c3846ee2bf1dd88e0c8
LucasPickering <lucas@lucaspickering.me>
2022-10-11 22:13:35 +0000

    Deploying to gh-pages from  @ 30e3ca961c6a88bd41f3d8da59e9c755bc1a195c 🚀

--- src/pages/posts/index.ts
+++ src/pages/posts/index.ts
@@ -3,8 +3,13 @@
  * I couldn't figure out how to dynamically list+load all the MDX files.
  */
 
+// Posts
+// These are sorted by date so order doesn't matter
 export * as bostonConcrete from "./boston-concrete.mdx";
-export * as laulud from "./laulud.mdx";
 export * as mtWashington from "./mt-washington.mdx";
+
+// Projects - these end up sorted alphabetically
+export * as laulud from "./laulud.mdx";
+export * as osrsCli from "./osrs-cli.mdx";
 export * as soze from "./soze.mdx";
 export * as terra from "./terra.mdx";
